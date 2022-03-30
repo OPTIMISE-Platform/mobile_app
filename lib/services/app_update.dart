40f498e82cd0a66d90e2ac51e55e9794939b8bac
franzmueller <franzmueller@users.noreply.github.com>
2022-03-30 14:08:48 +0200

    add auto updater

---
+++ lib/services/app_update.dart
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2022 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
+import 'package:mobile_app/exceptions/unexpected_status_code_exception.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../exceptions/no_network_exception.dart';
+
+class AppUpdater {
+  static final _client = http.Client();
+  static final _logger = Logger(
+    printer: SimplePrinter(),
+  );
+
+  late final updateSupported = _updateSupported();
+
+  late int currentBuild;
+  late int latestBuild;
+  late int downloadSize;
+
+  late String updateUrl;
+  late String localFile;
+
+  AppUpdater();
+
+  bool _updateSupported() {
+    if (Platform.isAndroid && dotenv.env["DISTRIBUTOR"] == "github" && dotenv.env["GITHUB_REPO"] != null && dotenv.env["version"] != null) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> updateAvailable() async {
+    if (!updateSupported) return false;
+
+    ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.none) throw NoNetworkException();
+
+    final url = "https://api.github.com/repos/" + dotenv.env["GITHUB_REPO"]! + '/releases?per_page=1';
+    var uri = Uri.parse(url);
+    if (url.startsWith("https://")) {
+      uri = uri.replace(scheme: "https");
+    }
+
+    final resp = await _client.get(uri);
+    if (resp.statusCode != 200) {
+      throw UnexpectedStatusCodeException(resp.statusCode);
+    }
+    final decoded = (json.decode(utf8.decode(resp.bodyBytes)) as List<dynamic>)[0] as Map<String, dynamic>;
+    latestBuild = int.parse((decoded["tag_name"] as String).split("+")[1]);
+    currentBuild = int.parse(dotenv.env["version"]!.split("+")[1]);
+
+    if (latestBuild > currentBuild) {
+      final asset = (decoded["assets"] as List<dynamic>).firstWhere((element) => element["name"] == "app-release.apk");
+      updateUrl = asset["browser_download_url"];
+      downloadSize = asset["size"];
+      return true;
+    }
+
+    return false;
+  }
+
+  Stream<double> downloadUpdate() async* {
+    var uri = Uri.parse(updateUrl);
+    if (updateUrl.startsWith("https://")) {
+      uri = uri.replace(scheme: "https");
+    }
+
+    final req = http.Request('GET', uri);
+    final resp = _client.send(req);
+    localFile = (await getApplicationSupportDirectory()).path + '/update.apk';
+
+    final List<List<int>> chunks = [];
+    int downloaded = 0;
+    double percentage = 0;
+
+    await for(final r in resp.asStream()) {
+      if (r.statusCode != 200) {
+        throw UnexpectedStatusCodeException(r.statusCode);
+      }
+
+      await for (final chunk in r.stream) {
+        percentage = downloaded / r.contentLength! * 100;
+        yield percentage;
+        chunks.add(chunk);
+        downloaded += chunk.length;
+      }
+
+      percentage = downloaded / r.contentLength! * 100;
+      yield percentage;
+
+      File file = File(localFile);
+      final Uint8List bytes = Uint8List(r.contentLength!);
+      int offset = 0;
+      for (List<int> chunk in chunks) {
+        bytes.setRange(offset, offset + chunk.length, chunk);
+        offset += chunk.length;
+      }
+      await file.writeAsBytes(bytes);
+    }
+  }
+}
--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -16,13 +16,16 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:mobile_app/services/app_update.dart';
 import 'package:mobile_app/services/cache_helper.dart';
 import 'package:mobile_app/widgets/app_bar.dart';
 import 'package:mobile_app/widgets/page_spinner.dart';
 import 'package:mobile_app/widgets/toast.dart';
+import 'package:open_file/open_file.dart';
 import 'package:provider/provider.dart';
 
 import '../app_state.dart';
+import '../exceptions/no_network_exception.dart';
 import '../services/auth.dart';
 import '../theme.dart';
 
@@ -31,44 +34,113 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const _appBar = MyAppBar("Settings");
+    const appBar = MyAppBar("Settings");
+    final appUpdater = AppUpdater();
+    final List<Widget> children = [
+      ListTile(
+        title: const Text("Switch Style"),
+        onTap: () => MyTheme.toggleTheme(context),
+      ),
+      const Divider(),
+      ListTile(
+        title: const Text("Clear Cache"),
+        onTap: () {
+          CacheHelper.clearCache();
+          Toast.showConfirmationToast(context, "Cache cleared, please restart App");
+        },
+      ),
+      const Divider(),
+      Consumer<AppState>(
+        builder: (context, state, child) => ListTile(
+          title: const Text("Logout"),
+          onTap: () async {
+            Navigator.push(
+                context,
+                platformPageRoute(
+                  context: context,
+                  builder: (context) => const PageSpinner("Logout"),
+                ));
+            try {
+              await Auth.logout(context, state);
+            } catch (e) {
+              Toast.showErrorToast(context, "Can't logout");
+            }
+          },
+        ),
+      ),
+    ];
+
+    if (appUpdater.updateSupported) {
+      children.addAll([
+        const Divider(),
+        ListTile(
+          title: const Text("Check Updates"),
+          onTap: () async {
+            late final bool updateAvailable;
+            try {
+              updateAvailable = await appUpdater.updateAvailable();
+            } on NoNetworkException {
+              Toast.showWarningToast(context, "Currently offline");
+              return;
+            } catch (e) {
+              Toast.showErrorToast(context, "Error checking for updates");
+              return;
+            }
+            if (!updateAvailable) {
+              Toast.showConfirmationToast(context, "Already up to date!");
+              return;
+            } else {
+              final proceed = await showPlatformDialog(
+                  context: context,
+                  builder: (context) => PlatformAlertDialog(
+                        title: const Text("Update now?"),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Current Build: " + appUpdater.currentBuild.toString()),
+                            Text("Latest Build: " + appUpdater.latestBuild.toString()),
+                            Text("Download size: " + (appUpdater.downloadSize / 1000000.0).toStringAsFixed(1) + " MB"),
+                          ],
+                        ),
+                        actions: [
+                          PlatformDialogAction(
+                            child: PlatformText('Cancel'),
+                            onPressed: () => Navigator.pop(context, false),
+                          ),
+                          PlatformDialogAction(child: PlatformText('OK'), onPressed: () => Navigator.pop(context, true))
+                        ],
+                      ));
+              if (proceed != true) {
+                return;
+              }
+              final stream = appUpdater.downloadUpdate().asBroadcastStream();
+              stream.listen(null, onDone: () => Navigator.pop(context));
+              await showPlatformDialog(
+                  context: context,
+                  builder: (context) => PlatformAlertDialog(
+                        title: const Text("Downloading Update..."),
+                        content: StreamBuilder<double>(
+                            stream: stream,
+                            initialData: 0,
+                            builder: (context, snapshot) {
+                              return Column(mainAxisSize: MainAxisSize.min, children: [
+                                LinearProgressIndicator(value: snapshot.data! / 100),
+                                Text(snapshot.data!.toStringAsFixed(2) + " %"),
+                              ]);
+                            }),
+                      ));
+              OpenFile.open(appUpdater.localFile);
+            }
+          },
+        ),
+      ]);
+    }
 
     return PlatformScaffold(
-      appBar: _appBar.getAppBar(context),
+      appBar: appBar.getAppBar(context),
       body: ListView(
-        children: [
-          ListTile(
-            title: const Text("Switch Style"),
-            onTap: () => MyTheme.toggleTheme(context),
-          ),
-          const Divider(),
-          ListTile(
-            title: const Text("Clear Cache"),
-            onTap: () {
-              CacheHelper.clearCache();
-              Toast.showConfirmationToast(context, "Cache cleared, please restart App");
-            },
-          ),
-          const Divider(),
-          Consumer<AppState>(
-            builder: (context, state, child) => ListTile(
-              title: const Text("Logout"),
-              onTap: () async {
-                Navigator.push(
-                    context,
-                    platformPageRoute(
-                      context: context,
-                      builder: (context) => const PageSpinner("Logout"),
-                    ));
-                try {
-                  await Auth.logout(context, state);
-                } catch (e) {
-                  Toast.showErrorToast(context, "Can't logout");
-                }
-              },
-            ),
-          ),
-        ],
+        children: children,
       ),
     );
   }
