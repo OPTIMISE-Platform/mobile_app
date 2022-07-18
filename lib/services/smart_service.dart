5f0ef34e0678c81ac5a58900f903df2e5ad09b64
franzmueller <franzmueller@users.noreply.github.com>
2022-07-18 08:45:35 +0200

    remove example code

--- lib/services/smart_service.dart
+++ lib/services/smart_service.dart
@@ -25,7 +25,6 @@ import 'package:mobile_app/models/smart_service.dart';
 import 'package:mobile_app/services/cache_helper.dart';
 
 import '../exceptions/unexpected_status_code_exception.dart';
-import '../models/content_variable.dart';
 import 'auth.dart';
 
 class SmartServiceService {
@@ -56,46 +55,6 @@ class SmartServiceService {
   }
 
   static Future<List<SmartServiceInstance>> getInstances(int limit, int offset) async {
-    /* TODO return [
-      SmartServiceInstance(
-          "dummy desc",
-          // TODO
-          "design_id",
-          "id-0",
-          "complete",
-          "release_id",
-          "user_id",
-          null,
-          true,
-          [
-            SmartServiceParameter("id-0", "differen"),
-            SmartServiceParameter(
-              "id-1",
-              3,
-            ),
-            SmartServiceParameter("id-2", 1337),
-            SmartServiceParameter("id-3", 0.4),
-            SmartServiceParameter("id-4", <dynamic>[0, 1]),
-            SmartServiceParameter("id-5", <dynamic>[0, 1]),
-            SmartServiceParameter(
-              "id-6",
-              null,
-            ),
-            SmartServiceParameter("id-7", <dynamic>[true, false]),
-            SmartServiceParameter(
-              "id-8",
-              false,
-            ),
-            SmartServiceParameter(
-              "id-9",
-              "value-a",
-            ),
-            SmartServiceParameter("id-10", "value-b")
-          ]),
-      SmartServiceInstance("dummy desc", "design_id", "id-0", "not ready", "release_id", "user_id", null, false, null),
-      SmartServiceInstance("dummy desc", "design_id", "id-0", "incomplete delete", "release_id", "user_id", "incomplete delete", false, null)
-    ]; */
-
     final String url = baseUrl + "/instances";
     final Map<String, String> queryParameters = {};
     queryParameters["limit"] = limit.toString();
@@ -182,73 +141,6 @@ class SmartServiceService {
   }
 
   static Future<List<SmartServiceExtendedParameter>> getReleaseParameters(String releaseId) async {
-    /* TODO return [
-
-      SmartServiceExtendedParameter("id-0", "label", "str-free", "default_value", "default_value", false, null, ContentVariable.STRING),
-      SmartServiceExtendedParameter("id-1", "label", "int-free", 1, 1, false, null, ContentVariable.INTEGER),
-      SmartServiceExtendedParameter(
-          "id-2",
-          "super long label i have no idea why anyone would need that much space for a label",
-          "description which is just very long and contains a lot of very important information like what this actually does and how you should configure all these awesome options which you really need to do right? no, because this service isnt just a smart service its actualöly a super smart service. its the result of 100 year long deep learning models that will improve your life and totally will get you laid daily",
-          null,
-          null,
-          false,
-          null,
-          ContentVariable.INTEGER),
-      SmartServiceExtendedParameter("id-3", "label", "float-free", 0.4, 0.4, false, null, ContentVariable.FLOAT),
-      SmartServiceExtendedParameter(
-          "id-4",
-          "super long label i have no idea why anyone would need that much space for a label",
-          "description which is just very long and contains a lot of very important information like what this actually does and how you should configure all these awesome options which you really need to do right? no, because this service isnt just a smart service its actualöly a super smart service. its the result of 100 year long deep learning models that will improve your life and totally will get you laid daily",
-          <dynamic>[0, 1],
-          <dynamic>[0, 1],
-          true,
-          null,
-          ContentVariable.INTEGER),
-      SmartServiceExtendedParameter("id-5", "label", "float-free-many", <dynamic>[0, 1], <dynamic>[0, 1], true, null, ContentVariable.FLOAT),
-      SmartServiceExtendedParameter(
-          "id-6",
-          "label",
-          "str-choose many",
-          null,
-          "1",
-          true,
-          [
-            SmartServiceParameterOption("", "a", "value-a"),
-            SmartServiceParameterOption("", "b", "value-b"),
-            SmartServiceParameterOption("", "c", "value-c"),
-          ],
-          ContentVariable.STRING),
-      SmartServiceExtendedParameter(
-          "id-7", "label", "bool-free-many", <dynamic>[false, true, false], <dynamic>[false, true, false], true, null, ContentVariable.BOOLEAN),
-      SmartServiceExtendedParameter("id-8", "label", "bool-free", false, false, false, null, ContentVariable.BOOLEAN),
-      SmartServiceExtendedParameter(
-          "id-9",
-          "label",
-          "str-choose one",
-          null,
-          null,
-          false,
-          [
-            SmartServiceParameterOption("", "a", "value-a"),
-            SmartServiceParameterOption("", "b", "value-b"),
-            SmartServiceParameterOption("", "c", "value-c"),
-          ],
-          ContentVariable.STRING),
-      SmartServiceExtendedParameter(
-          "id-10",
-          "label",
-          "str-choose one, default a",
-          "value-a",
-          "value-a",
-          false,
-          [
-            SmartServiceParameterOption("", "a", "value-a"),
-            SmartServiceParameterOption("", "b", "value-b"),
-            SmartServiceParameterOption("", "c", "value-c"),
-          ],
-          ContentVariable.STRING),
-    ]; */
     final String url = baseUrl + "/releases/" + releaseId + "/parameters";
 
     final headers = await Auth().getHeaders();
@@ -266,16 +158,6 @@ class SmartServiceService {
   }
 
   static Future<List<SmartServiceRelease>> getReleases(int limit, int offset) async {
-    /* TODO return [
-      SmartServiceRelease("1970-01-01T00:00:00Z", "daily", "design_id", "id", "kaputt release", "error"),
-      SmartServiceRelease(
-          "2223-01-01T00:00:00Z",
-          "description which is just very long and contains a lot of very important information like what this actually does and how you should configure all these awesome options which you really need to do right? no, because this service isnt just a smart service its actualöly a super smart service. its the result of 100 year long deep learning models that will improve your life and totally will get you laid daily",
-          "design_id",
-          "id",
-          "release name",
-          null)
-    ]; */
 
     final String url = baseUrl + "/releases";
     final Map<String, String> queryParameters = {};
@@ -297,14 +179,6 @@ class SmartServiceService {
   }
 
   static Future<SmartServiceRelease> getRelease(String id) async {
-    /* TODO return SmartServiceRelease(
-        "2223-01-01T00:00:00Z",
-        "description which is just very long and contains a lot of very important information like what this actually does and how you should configure all these awesome options which you really need to do right? no, because this service isnt just a smart service its actualöly a super smart service. its the result of 100 year long deep learning models that will improve your life and totally will get you laid daily",
-        "design_id",
-        "id",
-        "release name",
-        null); */
-
     final String url = baseUrl + "/releases/" + id;
 
     final headers = await Auth().getHeaders();
